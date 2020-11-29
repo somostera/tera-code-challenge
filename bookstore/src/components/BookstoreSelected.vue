d3c263f2f1f7c68aaffcaf96ca6ed90657c58831
Thais Ribeiro <thaisribeiro.mesquita@gmail.com>
2020-11-29 09:29:18 -0300

    Adicionado componente de seleção de livros de acordo com uma classificação

---
+++ bookstore/src/components/BookstoreSelected.vue
@@ -0,0 +1,20 @@
+<template>
+<select name="" id="">
+    <option value="">Filtros</option>
+     <option value="">Melhores Avaliados</option>
+      <option value="">Em Destaque</option>
+       <option value="">Livros Curtidos</option>
+       <option value="">Por Categoria</option>
+</select>
+</template>
+<script>
+export default {
+    props: ['mode']
+}
+</script>
+<style scoped>
+    .dark select {
+        background: #31303a;
+        color:#fff;
+    }
+</style>
