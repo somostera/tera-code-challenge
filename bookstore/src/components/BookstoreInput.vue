c7c6944923b6b7d381d0762701482600877e298f
Thais Ribeiro <thaisribeiro.mesquita@gmail.com>
2020-11-29 09:28:33 -0300

    Adicionado componente de campo de busca dos livros

---
+++ bookstore/src/components/BookstoreInput.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container-input">
+       <input type="text" placeholder="Procure por um livro" />
+  </div>
+</template>
+<script>
+export default {
+    data() {
+      return {
+            text: ''
+        }
+    },
+    props: ['mode']
+}
+</script>
+<style scoped>
+.dark input {
+    background: #31303a;
+    border-color: #121420;
+    color:#fff;
+}
+.container-input {
+}
+input {
+    padding: 5px;
+}
+</style>
