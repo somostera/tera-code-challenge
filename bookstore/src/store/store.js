cc1d3b9543447e5c8985dc4c108051457d7792f0
Thais Ribeiro <thaisribeiro.mesquita@gmail.com>
2020-11-30 14:49:57 -0300

    Adicionado arquivo de store da aplicação

---
+++ bookstore/src/store/store.js
@@ -0,0 +1,42 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from 'axios'
+import config from '../../config/config'
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    state: {
+        books: [],
+        search: ''
+    },
+
+    getters: {
+        allBooks(state) {
+            return state.books
+        },
+        getBooksFromIndex: state => id => {
+            let book = state.books.find((item, index )=> {
+                return(index == id);
+            });
+            return book;
+        },
+        getBooksFromName: state => name => {
+            let books = state.books.filter(book => book.name.includes(name));
+            return books;
+        }
+    },
+    actions: {
+        getBooks({commit}) {
+            axios.get(`${config.apiURL}/books`)
+            .then((response) => {
+                commit('SET_BOOKS', response.data)
+            })
+        }
+    },
+    mutations: {
+        SET_BOOKS(state, books) {
+        state.books = books
+        }
+    }
+})
