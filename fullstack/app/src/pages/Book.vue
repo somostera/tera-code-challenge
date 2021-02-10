5caf35798eca4f0f4be703355c4b5298477a2026
Alan Schio <6757777+schirrel@users.noreply.github.com>
2021-02-10 15:10:39 -0400

    feat: pages, skeleton

---
+++ fullstack/app/src/pages/Book.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div>
+      <form action="#" class="" @submit.prevent="save">
+        <div
+        >
+          <input
+            type="text"
+            id="nome"
+            v-model="model.nome"
+          />
+          <label for="nome">Nome...</label>
+        </div>
+ 
+        </div>
+
+        <div >
+          <button
+            type="submit"
+          >
+            Salvar
+          </button>
+
+          <button
+            :disabled="!model.id"
+            type="button"
+            @click="remove"
+          >
+            Apagar\
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+<script>
+import { books } from "@/services/api.js";
+
+export default {
+  name: "Book",
+  data: function () {
+    return {
+      model: {}
+    };
+  },
+  methods: {
+    async save() {
+      await books.save(this.model);
+      this.goToList();
+    },
+    remove() {
+      books.delete(this.model.key);
+      this.goToList();
+    },
+    goToList() {
+      this.$router.push({ name: "books" });
+    }
+  },
+};
+</script>
