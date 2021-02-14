5bcffaf7458a89a768925fa368ad706efa345436
quaresmateo <teoian.giovane@gmail.com>
2021-02-14 18:18:43 -0300

    Create bookshelf component

---
+++ fullstack/bookapp/src/components/Bookshelf.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="bookshelf">
+    <div class="book">
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/41TPrNDI50L._SX218_BO1,204,203,200_QL40_ML2_.jpg"
+        alt=""
+      />
+      <div class="info">
+        <div class="title">
+          <h2 class="book-title">Clean Archteture</h2>
+          <p class="book-author">Martin Fowler</p>
+          <p class="book-category">Technology</p>
+        </div>
+
+        <div class="like">&#60;3</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default { name: 'Bookshelf' }
+</script>
+
+<style lang="scss"></style>
--- fullstack/bookapp/src/views/Home.vue
+++ fullstack/bookapp/src/views/Home.vue
@@ -1,13 +1,47 @@
 <template>
-  <div class="home">Home</div>
+  <article>
+    <div class="filter-header">
+      <input
+        class="input"
+        id="input-search"
+        type="text"
+        placeholder="Procure um livro"
+      />
+      <input class="input" id="input-select" type="text" />
+    </div>
+    <p>Total: 40</p>
+    <Bookshelf />
+  </article>
 </template>
 
 <script>
 // @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
+import Bookshelf from '@/components/Bookshelf.vue'
 
 export default {
   name: 'Home',
-  components: {}
+  components: { Bookshelf }
 }
 </script>
+
+<style lang="scss" scoped>
+#input-search {
+  max-width: 500px;
+}
+
+#input-select {
+  max-width: 200px;
+  margin-left: 10px;
+}
+
+.filter-header {
+  display: flex;
+  justify-content: space-between;
+}
+
+@media only screen and (max-width: 600px) {
+  #input-select {
+    max-width: 150px;
+  }
+}
+</style>
