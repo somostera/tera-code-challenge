1ccbd4a3208567be8a4bc027f3c923a8a4b10e9f
debora-cristina <deboracrisf91@gmail.com>
2021-02-15 14:24:45 -0300

    :fire: - Removendo código do server.js

--- fullstack/src/server.js
+++ fullstack/src/server.js
@@ -1,17 +1,12 @@
 const port = 3000
 
-const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
-const allowCors = require('./config/cors')
 
 require('./config/mongodb')
 
 // Rotas da aplicação
 server.use(require('./routes'));
-
-
-server.use(allowCors)
 
 
 server.listen(process.env.PORT || port, function() {
