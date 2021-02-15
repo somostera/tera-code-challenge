6d79d88c3f7d88ecb90f83e2a9870cf5a22e43ef
quaresmateo <teoian.giovane@gmail.com>
2021-02-15 15:36:48 -0300

    Update store

--- fullstack/bookapp/src/store/index.js
+++ fullstack/bookapp/src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import { api } from '@/services'
+import { api } from '../services/index'
 const { localStorage } = window
 
 Vue.use(Vuex)
@@ -13,11 +13,25 @@ export default new Vuex.Store({
     book: {}
   },
   mutations: {
-    LOGIN(state, payload) {
-      state.login = payload
+    UPDATE_LOGIN(state, payload) {
+      state.logged = payload
+    },
+    UPDATE_USER(state, payload) {
+      state.user = Object.assign({}, state.user, payload)
+    },
+
+    UPDATE_BOOK(state, payload) {
+      state.book = Object.assign({}, state.book, payload)
     }
   },
   actions: {
+    getUser(context) {
+      return api.get('/user').then((response) => {
+        context.commit('UPDATE_USER', response.data)
+        context.commit('UPDATE_LOGIN', true)
+      })
+    },
+
     loginUser(_, payload) {
       return api
         .login({
@@ -25,7 +39,47 @@ export default new Vuex.Store({
           password: payload.password
         })
         .then((response) => {
-          localStorage.set('token', `Bearer ${response.data.data.token}`)
+          localStorage.setItem('token', `Bearer ${response.data.token}`)
+        })
+    },
+
+    createUser(_, payload) {
+      return api.post('/users', payload)
+    },
+
+    logoutUser(context) {
+      context.commit('UPDATE_USER', {})
+      localStorage.removeItem('token')
+      context.commit('UPDATE_LOGIN', false)
+    },
+
+    createBook(_, payload) {
+      return api.post('/books', payload)
+    },
+
+    getBook(context, payload) {
+      return api.get(`books/${payload}`).then((response) => {
+        context.commit('UPDATE_BOOK', response.data)
+      })
+    },
+
+    updateBook(context, payload) {
+      return api.put(`/books/${payload.id}`, payload).then((response) => {
+        context.commit('UPDATE_BOOK', response.data)
+      })
+    },
+
+    deleteBook(context, payload) {
+      return api.delete(`/books/${payload.id}`).then(() => {
+        context.commit('UPDATE_BOOK', {})
+      })
+    },
+
+    likeBook(context, payload) {
+      return api
+        .post(`/books/${payload.book.id}`, payload.deslike)
+        .then((response) => {
+          context.commit('UPDATE_BOOK', response.data)
         })
     }
   }
