f52f0aa19d58ff003938f05eab76e1e20252ea4f
quaresmateo <teoian.giovane@gmail.com>
2021-02-15 12:07:11 -0300

    Add login user

--- fullstack/bookapp/src/store/index.js
+++ fullstack/bookapp/src/store/index.js
@@ -1,11 +1,32 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { api } from '@/services'
+const { localStorage } = window
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  strict: true,
+  state: {
+    logged: false,
+    user: {},
+    book: {}
+  },
+  mutations: {
+    LOGIN(state, payload) {
+      state.login = payload
+    }
+  },
+  actions: {
+    loginUser(_, payload) {
+      return api
+        .login({
+          email: payload.email,
+          password: payload.password
+        })
+        .then((response) => {
+          localStorage.set('token', `Bearer ${response.data.data.token}`)
+        })
+    }
+  }
 })
