9c717c872bf4407d36cbb7d140b4afdbeab3b9f9
quaresmateo <teoian.giovane@gmail.com>
2021-02-16 15:48:27 -0300

    Creates side menu

--- fullstack/bookapp/src/components/NavBar.vue
+++ fullstack/bookapp/src/components/NavBar.vue
@@ -1,8 +1,22 @@
 <template>
-  <header class="navbar container">
-    <p class="nav-title">Livraria do cowboy</p>
-    <p class="dark-mode">Dark mode</p>
-  </header>
+  <div>
+    <header class="navbar container">
+      <div class="left-side">
+        <button id="menu-mobile" @click="openNav()">
+          <img src="@/assets/menu-light.svg" />
+        </button>
+        <p class="nav-title">Livraria do cowboy</p>
+      </div>
+      <p class="dark-mode">Dark mode</p>
+    </header>
+
+    <div id="mySidenav" class="sidenav">
+      <a href="javascript:void(0)" class="closebtn" @click="closeNav()"
+        >&times;</a
+      >
+      <router-link to="/formulario">Adicionar Livro</router-link>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -10,6 +24,16 @@ export default {
   name: 'NavBar',
   data() {
     return {}
+  },
+
+  methods: {
+    openNav() {
+      document.getElementById('mySidenav').style.width = '250px'
+    },
+
+    closeNav() {
+      document.getElementById('mySidenav').style.width = '0'
+    }
   }
 }
 </script>
@@ -19,6 +43,12 @@ export default {
 
 .dark-mode {
   font-size: 18px;
+}
+
+.left-side {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 }
 
 .navbar {
@@ -44,6 +74,56 @@ export default {
   font-size: 28px;
 }
 
+#menu-mobile {
+  display: inline-block;
+  background: none;
+  border: none;
+  margin: 10px 20px 0 0;
+}
+
+#menu-mobile:focus {
+  outline: none;
+}
+#menu-mobile img {
+  pointer-events: none;
+}
+
+/* Side menu */
+
+.sidenav {
+  height: 100%;
+  width: 0;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  background-color: $primary;
+  overflow-x: hidden;
+  transition: 0.2s;
+  padding-top: 60px;
+}
+
+.sidenav a {
+  padding: 8px 8px 8px 32px;
+  text-decoration: none;
+  font-size: 25px;
+  color: #f1f1f1;
+  display: block;
+  transition: 0.3s;
+}
+
+.sidenav a:hover {
+  color: #818181;
+}
+
+.sidenav .closebtn {
+  position: absolute;
+  top: 0;
+  right: 25px;
+  font-size: 36px;
+  margin-left: 50px;
+}
+
 @media only screen and (max-width: 600px) {
   .nav-title {
     font-size: 18px;
@@ -51,6 +131,13 @@ export default {
 
   .dark-mode {
     font-size: 14px;
+  }
+
+  .sidenav {
+    padding-top: 15px;
+  }
+  .sidenav a {
+    font-size: 18px;
   }
 }
 </style>
