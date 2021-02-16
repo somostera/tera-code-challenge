83acd6b71ab8a00cb6bc11642ae1be821b4d3abd
debora-cristina <deboracrisf91@gmail.com>
2021-02-15 21:32:48 -0300

    :bug: - Correção autorização headers

--- fullstack/src/config/cors.js
+++ fullstack/src/config/cors.js
@@ -1,8 +1,10 @@
 module.exports = function() {
     return function(req, res, next) {
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-        res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 
+        'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
         next();
     };
   }
