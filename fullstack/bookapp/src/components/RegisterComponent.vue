255ff5620d6c2e2716416e305f25ddd97a94078a
quaresmateo <teoian.giovane@gmail.com>
2021-02-15 23:10:33 -0300

    Uptade RegisterComponent

--- fullstack/bookapp/src/components/RegisterComponent.vue
+++ fullstack/bookapp/src/components/RegisterComponent.vue
@@ -1,20 +1,35 @@
 <template>
-  <form>
+  <form id="register" @submit="login" method="post">
     <div class="form-content">
       <label class="label" for="input-name">Nome:</label>
-      <input class="input" id="input-name" type="text" />
+      <input class="input" id="input-name" type="text" v-model="data.name" />
 
       <label class="label" for="input-username">* Nome de usuário:</label>
-      <input class="input" id="input-username" type="text" />
+      <input
+        class="input"
+        id="input-username"
+        type="text"
+        v-model="data.username"
+      />
 
       <label class="label" for="input-email">* Email:</label>
-      <input class="input" id="input-email" type="email" />
+      <input class="input" id="input-email" type="email" v-model="data.email" />
 
       <label class="label" for="input-password">* Senha:</label>
-      <input class="input" id="input-password" type="password" />
+      <input
+        class="input"
+        id="input-password"
+        type="password"
+        v-model="data.password"
+      />
     </div>
     <div class="form-footer">
       <button class="button" type="submit">Criar conta</button>
+    </div>
+    <div class="erro-info">
+      <div v-for="(error, index) in errors" :key="index">
+        {{ error }}
+      </div>
     </div>
   </form>
 </template>
@@ -23,7 +38,41 @@
 export default {
   name: 'RegisterComponent',
   data() {
-    return {}
+    return {
+      data: {},
+      errors: []
+    }
+  },
+  methods: {
+    login(e) {
+      if (this.data.email && this.data.password && this.data.username) {
+        this.$store
+          .dispatch('createUser', this.data)
+          .then(() => {
+            this.$store.dispatch('getUser')
+            this.$router.push('/')
+          })
+          .catch((error) => {
+            this.errors.push(error)
+          })
+      }
+
+      this.errors = []
+
+      if (!this.data.email) {
+        this.errors.push('Por favor, insira o email')
+      }
+
+      if (!this.data.email) {
+        this.errors.push('Por favor, insira o nome de usuário')
+      }
+
+      if (!this.data.password) {
+        this.errors.push('Por favor, insira a senha')
+      }
+
+      e.preventDefault()
+    }
   }
 }
 </script>
