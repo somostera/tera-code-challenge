c03f8248678fba9af0db86f1a7f710c07ca3e565
Bianca Correa <bee.correa@hotmail.com>
2021-06-26 01:45:23 -0300

    fix: alphabetical order was inverted

--- frontend/src/components/Book/BookFilters.vue
+++ frontend/src/components/Book/BookFilters.vue
@@ -105,8 +105,8 @@ export default {
     },
     byName(books) {
       return books.sort((a, b) => {
-        if (a.name > b.name) return -1;
-        if (a.name < b.name) return 1;
+        if (a.name < b.name) return -1;
+        if (a.name > b.name) return 1;
         return 0;
       });
     },
