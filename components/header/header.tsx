35c04d8f7f7ef096a7a4dc5aa912d6d63cd46fe3
JeanSilva999 <jean.luca.dev@outlook.com>
2025-04-08 10:30:36 -0300

    fix(Header): adjust base path to fetch logo image

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -7,7 +7,7 @@ export function Header({ children }: PropsWithChildren) {
     <header className="bg-black">
       <section className="flex min-h-[90px] flex-col items-center gap-2 p-4 md:flex-row">
         <Link href="/" className="cursor-pointer">
-          <Image src="logo.svg" width={92} height={48} alt="Tera" />
+          <Image src="/logo.svg" width={92} height={48} alt="Tera" />
         </Link>
         <section className="flex flex-grow justify-center">{children}</section>
       </section>
