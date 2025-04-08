139d99d42c2c3b853612649a52941ff4529480f3
JeanSilva999 <jean.luca.dev@outlook.com>
2025-04-08 15:20:33 -0300

    feat(getCourses): set cache and revalidate cache config

--- services/get-courses.ts
+++ services/get-courses.ts
@@ -5,7 +5,10 @@ export type GetCoursesResponse = { courses: ICourse[] };
 
 export async function getCourses(): Promise<GetCoursesResponse> {
   try {
-    const response = await fetch(`${BASE_URL}/api/courses`);
+    const response = await fetch(`${BASE_URL}/api/courses`, {
+      cache: "force-cache",
+      next: { revalidate: 3600 },
+    });
     return await response.json();
   } catch (error) {
     return await Promise.reject(error);
