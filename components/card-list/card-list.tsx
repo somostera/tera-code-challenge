ddf5487d3f9a5489aa6e60b0f4bc0a83818b5177
JeanSilva999 <jean.luca.dev@outlook.com>
2025-04-08 10:12:17 -0300

    refactor(CardList): create fallback ui if search is not found

--- components/card-list/card-list.tsx
+++ components/card-list/card-list.tsx
@@ -5,6 +5,15 @@ import { useContextState } from "@/contexts/courses";
 
 export function CardList() {
   const { filteredCourses: courses } = useContextState();
+
+  if (!courses.length) {
+    return (
+      <p className="mx-auto w-[90%] text-center text-xl md:text-2xl">
+        Não foi possível encontrar resultados para esta pesquisa.
+      </p>
+    );
+  }
+
   return (
     <section className="mx-auto grid max-w-9/12 grid-cols-[auto] place-content-center gap-5 p-2.5 md:grid-cols-[300px_300px] lg:grid-cols-[320px_320px_320px]">
       {courses.map((course) => (
