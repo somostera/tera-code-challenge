14e964abfeebb4bc1affb5038e5f115da842fbbb
romaopedro199 <romaopedro199@gmail.com>
2025-04-09 17:09:36 -0300

    chore: change home banner text

--- components/modules/home/banner/index.tsx
+++ components/modules/home/banner/index.tsx
@@ -1,18 +1,21 @@
 const HomeBanner = () => {
   return (
     <div
-      className="bg-amber-50 flex items-center justify-center py-40 px-5"
+      className="bg-amber-50 flex items-center justify-center py-20 lg:py-40 px-5"
       style={{
         backgroundImage: "url(/images/banner.jpg)",
-        backgroundPosition: "right",
+        backgroundPosition: "center",
         backgroundSize: "cover",
       }}
     >
       <div className="mx-auto text-center">
-        <h1 className="text-blue-950 text-[48px] font-semibold">
+        <h1 className="text-blue-950 text-[24px] lg:text-[48px] font-semibold mb-3">
           Explore Nossos Cursos
         </h1>
-        <p className="text-sm text-gray-600">Lorem Ipsum</p>
+        <p className="text-sm text-gray-600">
+          Descubra conteúdos criados por especialistas para acelerar sua jornada
+          de aprendizado
+        </p>
       </div>
     </div>
   );
