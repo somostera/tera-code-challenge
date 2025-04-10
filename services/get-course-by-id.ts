149e687b59ae2567f6cf44245acac1874bc10490
JeanSilva999 <jean.luca.dev@outlook.com>
2025-04-10 14:18:52 -0300

    chore(getCourseById): set cache and revalidate for request

--- services/get-course-by-id.ts
+++ services/get-course-by-id.ts
@@ -7,7 +7,10 @@ export async function getCourseById(
   id: number
 ): Promise<GetCourseByIdResponse> {
   try {
-    const response = await fetch(`${BASE_URL}/api/courses/${id}`);
+    const response = await fetch(`${BASE_URL}/api/courses/${id}`, {
+      cache: "force-cache",
+      next: { revalidate: 3600 },
+    });
     return await response.json();
   } catch (error) {
     return await Promise.reject(error);
