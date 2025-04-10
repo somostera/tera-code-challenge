2f185d8bf9cab16e0e8ce1b4ca82b93c99b3e360
romaopedro199 <romaopedro199@gmail.com>
2025-04-10 09:26:20 -0300

    chore: finish home page cypress tests

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,7 +1,11 @@
-describe("Select Component", () => {
+import coursesData from "../../data/courses.json";
+
+const loadingTime = 1500;
+
+describe("Home page tests", () => {
   beforeEach(() => {
     cy.visit("/");
-    cy.wait(1500);
+    cy.wait(loadingTime);
   });
 
   it("render filters", () => {
@@ -35,7 +39,7 @@ describe("Select Component", () => {
 
     cy.get("button").contains("Design").click();
 
-    cy.wait(1500);
+    cy.wait(loadingTime);
 
     categorySelectBtn.should("contain", "Design");
   });
@@ -46,8 +50,32 @@ describe("Select Component", () => {
 
     cy.get("button").contains("Intermediário").click();
 
-    cy.wait(1500);
+    cy.wait(loadingTime);
 
     levelSelectBtn.should("contain", "Intermediário");
+  });
+
+  it("render all courses", () => {
+    coursesData.courses.forEach((course) => {
+      cy.get("h3").contains(course.title).should("be.visible");
+    });
+  });
+
+  it("render filtered courses", () => {
+    const categorySelectBtn = cy.get("button[data-cy='category']");
+    categorySelectBtn.click();
+
+    cy.get("button").contains("Design").click();
+
+    cy.wait(loadingTime);
+
+    categorySelectBtn.should("contain", "Design");
+
+    coursesData.courses
+      .filter((course) => course.category === "Design")
+      .forEach((course) => {
+        cy.get("h3").contains(course.title).should("be.visible");
+        cy.get("h3").contains("Dados").should("not.exist");
+      });
   });
 });
