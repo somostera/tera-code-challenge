83e1e821266a1fbd98b0f0a02da77d5f58f0b37f
JeanSilva999 <jean.luca.dev@outlook.com>
2025-04-11 00:07:55 -0300

    chore: configure base url for cypress tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    baseUrl: process.env.NEXT_PUBLIC_BASE_URL ?? "http://localhost:3000",
     setupNodeEvents() {
       // implement node event listeners here
     },
