d2622a402089a3a0a46e84cff342cf696ad5cb90
romaopedro199 <romaopedro199@gmail.com>
2025-04-10 21:36:01 -0300

    chore: update unit tests and add unit tests for select component

--- __tests__/actions/levels.test.ts
+++ __tests__/actions/levels.test.ts
@@ -1,18 +1,26 @@
 import fetchLevels from "@/actions/levels";
+import { LEVELS } from "@/constants/levels";
 import coursesData from "@/data/courses.json";
+const levelsSet = new Set<string>();
+
+coursesData.courses.forEach((course) => {
+  levelsSet.add(course.level);
+});
+
+const uniqueLevels = Array.from(levelsSet).sort();
+const expectedLevels = uniqueLevels.map((level) => ({
+  label: LEVELS[level],
+  value: level,
+}));
 
 describe("fetchLevels", () => {
   it("should return an array of unique levels sorted alphabetically", async () => {
     const result = await fetchLevels();
-    const expectedLevels = Array.from(
-      new Set(coursesData.courses.map((course) => course.level))
-    ).sort();
 
     expect(result).toEqual(expectedLevels);
   });
 
   it("should handle duplicate levels correctly", async () => {
-    const expectedLevels = ["avancado", "iniciante", "intermediario"];
     const result = await fetchLevels();
 
     expect(result).toEqual(expectedLevels);
